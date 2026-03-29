dd2fd5d55c19f0f7fa500a6adb994490f3b63f93
David Peek <dpeek@users.noreply.github.com>
2026-03-30 10:46:44 +1100

    Minor

--- lib/app/src/web/main.tsx
+++ lib/app/src/web/main.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
+import "@io/web/global.css";
 import { router } from "./router";
 
 const container = document.getElementById("app");
